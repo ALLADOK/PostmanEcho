f3fe0096faa5052e06a6fa14c75c13b334cf8bc2
ALLA <all.tarasowa808@yandex.ru>
2024-07-06 22:06:16 +0300

    Reformat test1

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -12,11 +12,11 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Тестовые данные")
+                .body("some data")
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Тестовые данные"));
+                .body("data", equalTo("some value"));
     }
 }
